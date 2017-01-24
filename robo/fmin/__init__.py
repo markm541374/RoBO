041f88b1b913b7ca5742b9f9d975c3b91b501773
markm541374 <mark.mcleod@eng.ox.ac.uk>
2017-01-24 15:36:31 +0000

    alternative kernel

--- robo/fmin/__init__.py
+++ robo/fmin/__init__.py
@@ -1,5 +1,5 @@
 from .bayesian_optimization import bayesian_optimization
 from .random_search import random_search
-from .fabolas import fabolas
+from .fabolas import fabolas, fabolas_mod
 from .mtbo import mtbo
 from .bohamiann import bohamiann
