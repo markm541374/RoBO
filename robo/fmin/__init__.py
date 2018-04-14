bbb3cf435517e47e3e2070a3ef399811397585ae
markm541374 <mark.mcleod@eng.ox.ac.uk>
2018-04-14 16:18:30 +0100

    remove import bohammian due to theano errors

--- robo/fmin/__init__.py
+++ robo/fmin/__init__.py
@@ -2,4 +2,4 @@ from .bayesian_optimization import bayesian_optimization
 from .random_search import random_search
 from .fabolas import fabolas, fabolas_mod
 from .mtbo import mtbo
-from .bohamiann import bohamiann
+#from .bohamiann import bohamiann
